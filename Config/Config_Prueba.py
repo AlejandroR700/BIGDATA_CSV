0595108e00e1276845eeb7606f38c413bdd0d31c
AlejandroR700 <111135574+AlejandroR700@users.noreply.github.com>
2025-09-03 13:18:24 +0000

    bigdata_csv

---
+++ Config/Config_Prueba.py
@@ -0,0 +1,7 @@
+class Config:
+
+    ROOT = "/workspaces/Prueba_BIG_DATA"  
+
+    SOURCES_DIR = ROOT + "/Extract"
+    INPUT_FILE = SOURCES_DIR + "/business-financial-csv.csv"
+    OUTPUT_FILE = SOURCES_DIR + "/business-financial-csv_Limpio.csv"
